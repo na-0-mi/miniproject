d1c51d3a1cb73495bfc19a82318bbd7bbe13deaf
LAPTOP-F71LMNFC\gwg03 <gwg03185@gmail.com>
2022-07-13 09:26:18 +0900

    구,동 텍스로 바꾸기

--- app.py
+++ app.py
@@ -10,42 +10,44 @@ db = client.miniprj
 
 @app.route('/posting')
 def home():
-    words = list(db.dong.find({}, {"_id": False}))
+    words = list(db.mini.find({}, {"_id": False}))
     return render_template('posting.html', words=words)
 
-def main():
-    # DB에서 저장된 단어 찾아서 HTML에 나타내기
-    words = list(db.words.find({}, {"_id": False}))
-    return render_template("posting.html", words=words)
+# def main():
+#     # DB에서 저장된 단어 찾아서 HTML에 나타내기
+#     words = list(db.words.find({}, {"_id": False}))
+#     return render_template("main.html", words=words)
 @app.route('/main')
 def main():
-    return render_template("main.html")
+    words = list(db.mini.find({}, {"_id": False}))
+    return render_template("main.html",words=words)
 
-@app.route("/dong", methods=["POST"])
-def dong_post():
-    # gu_recieve = request.form['gu_recieve']
-    # dong_reveive = request.form['dong_recive']
+
+@app.route("/mini", methods=["POST"])
+def mini_post():
+    gu_receive = request.form['gu_give']
+    dong_receive = request.form['dong_give']
     chung_receive = request.form['chung_give']
     ann_receive = request.form['ann_give']
     gyo_receive = request.form['gyo_give']
     pyun_receive = request.form['pyun_give']
 
     doc = {
-        # 'gu':gu_receive,
-        # 'dong':dong_receive,
+        'gu':gu_receive,
+        'dong':dong_receive,
         'chung':chung_receive,
         'ann':ann_receive,
         'gyo':gyo_receive,
         'pyun':pyun_receive,
     }
-    db.dong.insert_one(doc)
+    db.mini.insert_one(doc)
 
     return jsonify({'msg': '저장 완료!'})
 
-@app.route("/dong", methods=["GET"])
-def dong_get():
-    dong_list = list(db.dong.find({}, {'_id': False}))
-    return jsonify({'dong':dong_list})
+@app.route("/mini", methods=["GET"])
+def mini_get():
+    mini_list = list(db.mini.find({}, {'_id': False}))
+    return jsonify({'mini':mini_list})
 
 
 if __name__ == '__main__':
